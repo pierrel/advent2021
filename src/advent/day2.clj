92d2e9c9cf56229d9ef846002114689bcad551c0
Pierre Larochelle <pierre@quirozlarochelle.com>
2021-12-02 19:16:08 -0800

    Day 2 part 2

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -15,7 +15,7 @@
     [(keyword str-dir)
      (Integer/parseInt str-count)]))
 
-(defn move-to-change
+(defn move-to-change-basic
   "Returns a map of movement as change to either :horiz or :depth"
   [[dir count]]
   (case dir
@@ -23,11 +23,32 @@
     :down {:depth count}
     :up {:depth (* -1 count)}))
 
+(defn aimed-move-to-change
+  "Returns a map of the change based on aim."
+  [[type change] pos]
+  (case type
+    :down {:aim change}
+    :up {:aim (* -1 change)}
+    :forward {:horiz change
+              :depth (* (:aim pos) change)}))
+
 (comment
+  ;; Part 2
+  (let [moves (->> (utils/file-to-seq "input/day2.txt")
+                   (map parse-move))
+        updated-pos (reduce (fn [pos move]
+                              (merge-with +
+                                          pos
+                                          (aimed-move-to-change move pos)))
+                            init-pos
+                            moves)]
+    (apply * (map updated-pos
+                  [:depth :horiz])))
+
   ;; Part 1
   (let [changes (->> (utils/file-to-seq "input/day2.txt")
                      (map parse-move)
-                     (map move-to-change))
+                     (map move-to-change-basic))
         updated-pos (reduce (fn [current change]
                               (merge-with +
                                           current
