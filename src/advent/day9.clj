c9461fd31a119ee2c6bc8d27443505f8fc8f2f85
Pierre Larochelle <pierre@quirozlarochelle.com>
2021-12-16 08:10:15 -0800

    Day 9 part 2

--- src/advent/day9.clj
+++ src/advent/day9.clj
@@ -1,45 +1,26 @@
 (ns advent.day9
   (:require [advent.utils :as utils]))
 
+(defrecord Point [x y])
+(defrecord ElementPoint [element point])
 
-(defn seq-around 
-"Given 3 seqs returns a lazy seq of the middles with each of the adjacent elements
- in order of up, left, down, right.
- [1 2 3]
- [3 2 1]
- [5 6 7]
- => [[3 [1 5 nil 2]] [2 [2 6 3 1]] ...]"
-  [top middle bottom]
-  (map-indexed (fn [idx elem]
-                 [elem [(nth top idx nil)
-                        (nth bottom idx nil)
-                        (nth middle (dec idx) nil)
-                        (nth middle (inc idx) nil)]])
-               middle))
+(defn points-around [point]
+  (let [{:keys [x y]} point]
+    [(Point. x (dec y))
+     (Point. x (inc y))
+     (Point. (dec x) y)
+     (Point. (inc x) y)]))
 
-(comment
-  (= [[3 [1 5 nil 2]]
-      [2 [2 6 3 1]]
-      [1 [3 7 2 nil]]]
-     (seq-around [1 2 3] 
-                 [3 2 1] 
-                 [5 6 7])))
+(defn elem-at [point seq2d]
+  (nth (nth seq2d (:y point) '())
+       (:x point) nil))
 
-  (= (seq-around []
-                 [1 2 3]
-                 [3 2 1])
-     [[1 [nil 3 nil 2]]
-      [2 [nil 2 1 3]]
-      [3 [nil 1 2 nil]]])
-
-(defn three-seq-groups
-  "Returns a list of three-line groups."
-  [orig]
-  (map (fn [idx]
-         [(nth orig (dec idx) [])
-          (nth orig idx [])
-          (nth orig (inc idx) [])])
-       (-> orig count range)))
+(defn elem-points-around [point seq2d]
+  (->> point
+       points-around
+       (map (fn [point]
+              (ElementPoint. (elem-at point seq2d)
+                             point)))))
 
 (defn is-smaller?
   "returns true if `elem` is smaller than all elems in `than-coll`"
@@ -47,37 +28,86 @@
   (every? #(< elem %)
           than-coll))
 
+(defn in-basin? [basin-elem-pt elem-pt]
+  (let [elem (:element elem-pt)]
+    (and (not (nil? elem))
+         (not= 9 elem)
+         (< (:element basin-elem-pt)
+            elem))))
+
+(defn surrounding-basin [sink seq2d]
+  (filter (partial in-basin? sink)
+          (elem-points-around (:point sink) seq2d)))
+
+(defn basin-in [sink seq2d]
+  (loop [basins #{}
+         to-check #{sink}]
+    (if (empty? to-check)
+      basins
+      (let [surrounding-check (->> to-check
+                                   (map #(surrounding-basin % seq2d))
+                                   flatten
+                                   set)
+            surrounding-unchecked (clojure.set/difference surrounding-check
+                                                          basins)]
+        (recur (clojure.set/union basins to-check)
+               surrounding-unchecked)))))
 
 (comment
+;; part 2
+  (let [input (->> "input/day9.txt"
+                   utils/file-to-seq
+                   (map #(clojure.string/split % #""))
+                   (map (fn [str-seq]
+                          (map #(Integer/parseInt %) str-seq))))
+        elems-around-ptd (map-indexed
+                          (fn [y-idx seq]
+                            (map-indexed
+                             (fn [x-idx elem]
+                               (let [point (Point. x-idx y-idx)]
+                                 [(ElementPoint. elem point)
+                                  (map #(elem-at % input) (points-around point))]))
+                             seq))
+                          input)
+        basins (->> elems-around-ptd
+                    (reduce concat)
+                    (reduce (fn [sinks [element-point elements-around]]
+                              (if (is-smaller? (:element element-point)
+                                               (filter (complement nil?) elements-around))
+                                (conj sinks element-point)
+                                sinks))
+                            [])
+                    (map (fn [sink]
+                           (basin-in sink input))))]
+    (->> basins
+         (map count)
+         (sort >)
+         (take 3)
+         (reduce * 1)))
+
+
 ;; part 1
-  (->> "input/day9.txt"
-       utils/file-to-seq
-       (map #(clojure.string/split % #""))
-       (map (fn [str-seq] (map #(Integer/parseInt %) str-seq)))
-       three-seq-groups
-       (map #(apply seq-around %))
-       (reduce concat [])
-       (reduce (fn [local-smallest [elem surrounding-elems]]
-                 (if (is-smaller? elem 
-                                  (filter (complement nil?) surrounding-elems))
-                   (conj local-smallest elem)
-                   local-smallest))
-               [])
-       (map inc)
-       (apply +))
-  
-  ;; testing
-  (->> [[1 2 3 4]
-        [4 3 2 1]
-        [5 6 7 8]
-        [8 7 6 5]]
-       three-seq-groups
-       (map #(apply seq-around %))
-       (reduce concat [])
-       (reduce (fn [local-smallest [elem surrounding-elems]]
-                 (if (is-smaller? elem 
-                                  (filter (complement nil?) surrounding-elems))
-                   (conj local-smallest elem)
-                   local-smallest))
-               []))
-  )
+  (let [input (->> "input/day9.txt"
+                   utils/file-to-seq
+                   (map #(clojure.string/split % #""))
+                   (map (fn [str-seq]
+                          (map #(Integer/parseInt %) str-seq))))
+        elems-and-around (map-indexed
+                          (fn [y-idx seq]
+                            (map-indexed
+                             (fn [x-idx elem]
+                               [elem (->> (Point. x-idx y-idx)
+                                          points-around
+                                          (map #(elem-at % input))
+                                          (filter (complement nil?)))])
+                             seq))
+                          input)]
+    (->> elems-and-around
+         (reduce concat [])
+         (reduce (fn [sinks elem-and-around]
+                   (if (apply is-smaller? elem-and-around)
+                     (conj sinks (first elem-and-around))
+                     sinks))
+                 [])
+         (map inc)
+         (reduce + 0))))
