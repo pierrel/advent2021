14c0b1a1943db94a051a70c20aa2b201a9efeef1
Pierre Larochelle <pierre@quirozlarochelle.com>
2021-12-17 08:51:45 -0800

    Day 10 part 2

--- src/advent/day10.clj
+++ src/advent/day10.clj
@@ -7,6 +7,12 @@
    "}" 1197
    ">" 25137})
 
+(def completion-points
+  {")" 1
+   "]" 2
+   "}" 3
+   ">" 4})
+
 (def pairs
   {"(" ")"
    "[" "]"
@@ -31,7 +37,42 @@
                      (rest rem))
               cur))))))
 
+(defn complete-chunks
+"Returns nil for corrupt lines. Returns the seq of characters to complete incomplete chunks."
+  [line]
+  (loop [chunk-starts '()
+         rem line]
+      (if (empty? rem)
+        (map pairs chunk-starts)
+        (let [cur (first rem)
+              most-recent-opening (first chunk-starts)
+              is-opening (get pairs cur)]
+          (if is-opening
+            (recur (conj chunk-starts cur)
+                   (rest rem))
+            (if (= cur (get pairs most-recent-opening))
+              (recur (rest chunk-starts)
+                     (rest rem))
+              nil))))))
+
 (comment
+;; part 2
+(let [sorted-points (->> "input/day10.txt"
+                         utils/file-to-seq
+                         (map (fn [line]
+                                (clojure.string/split line #"")))
+                         (map complete-chunks)
+                         (filter (complement nil?))
+                         (map #(reduce (fn [total char]
+                                         (+ (get completion-points char)
+                                            (* 5 total)))
+                                       0
+                                       %))
+                         sort)]
+  (nth sorted-points (/ (count sorted-points) 2)))
+
+
+;; part 1
   (->> "input/day10.txt"
        utils/file-to-seq
        (map (fn [line]
